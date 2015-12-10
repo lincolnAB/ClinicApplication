00f979e59923cb149948c3a5afca5de970137b37
lincolnAB <gamemail9241@gmail.com>
2015-12-10 13:46:29 +0100

    Next step.
    Some javasript added.

---
+++ build/web/resources/javascript/javascript.js
@@ -0,0 +1,26 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function alertSize() {
+                var myWidth = 0, myHeight = 0;
+                if (typeof (window.innerWidth) == 'number') {
+                    // Non-IE
+                    myWidth = window.innerWidth;
+                    myHeight = window.innerHeight;
+                } else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
+                    // IE 6+ in 'standards compliant mode'
+                    myWidth = document.documentElement.clientWidth;
+                    myHeight = document.documentElement.clientHeight;
+                } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
+                    // IE 4 compatible
+                    myWidth = document.body.clientWidth;
+                    myHeight = document.body.clientHeight;
+                }
+
+                window.alert('Width = ' + myWidth);
+                window.alert('Height = ' + myHeight);
+            }
+
